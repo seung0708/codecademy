b4a06d56ecf155921c5dbaa48cccbb02ff7e1fc3
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-03-25 18:30:21 -0700

    trying to figure out why search isnt working all of a sudden

--- full-stack-path/practice-project/jammming/src/api/spotifyAPI.js
+++ full-stack-path/practice-project/jammming/src/api/spotifyAPI.js
@@ -1,11 +1,11 @@
 export const fetchResultsData = async (accessToken, input) => {
+    console.log(input)
     const response = await fetch(`https://api.spotify.com/v1/search?q=${input}&type=track`, {
         method: 'GET',
         headers: { 
             Authorization: 'Bearer ' + accessToken 
         }
     })
-
     const {tracks} = await response.json()
     console.log(tracks)
     return tracks?.items;
