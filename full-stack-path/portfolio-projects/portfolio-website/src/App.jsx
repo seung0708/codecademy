d5911135ccc482a0d2a3efad014d808a6f866d23
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-08-05 19:52:37 -0700

    re-styled about component

--- full-stack-path/portfolio-projects/portfolio-website/src/App.jsx
+++ full-stack-path/portfolio-projects/portfolio-website/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer';
 import Hero from './components/Hero/Hero';
-import About from './components/About';
+import About from './components/About/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
