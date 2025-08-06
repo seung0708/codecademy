168ea25578f88462a15a81d12f484fdf6babc5c1
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-08-05 19:32:12 -0700

    updated the styles for App, Header, Navbar, and Hero

--- full-stack-path/portfolio-projects/portfolio-website/src/App.jsx
+++ full-stack-path/portfolio-projects/portfolio-website/src/App.jsx
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
-import Header from './components/Header'
-import Main from './components/Main';
+import './App.css'
+import Header from './components/Header/Header'
 import Footer from './components/Footer';
+import Hero from './components/Hero/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true)
   return (
-    <>
-      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Main darkMode={darkMode} />
+    <div className='app'>
+      <Header />
+      <main> 
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
       <Footer />
-    </>
+    </div>
   )
 }
 
---
+++ full-stack-path/portfolio-projects/portfolio-website/src/components/Hero/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-text">
+                <h1>Hi, I'm Seung Kim</h1>
+                <h2 className='subtitle'>Full-Stack Developer</h2>
+                <p className='description'>I design and build modern, user-friendly websites</p>
+                <a href='#projects' className='cta-button' aria-label='View My Work'>View My Work</a>
+            </div>
+            <div className='hero-image'>
+                <img src='../../../public/assets/images/IMG_20190312_191356_018.jpg'  />
+            </div> 
+            
+        </section>
+  )
+}
+
+export default Hero
