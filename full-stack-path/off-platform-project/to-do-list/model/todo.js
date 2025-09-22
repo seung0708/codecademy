22c06d7ac947a0917cd98aafc4b6fd2217642952
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-09-22 13:35:18 -0700

    completed model

---
+++ full-stack-path/off-platform-project/to-do-list/model/todo.js
@@ -0,0 +1,30 @@
+const pool = require('./database.js')
+
+const create = (description) => {
+    pool.query(`
+        INSERT INTO todo (description)
+        VALUES ($1) 
+        RETURNING *
+        `, [description])
+}
+
+const get = () => {
+    pool.query(`
+        SELECT * 
+        FROM todo
+        `)
+}
+
+const remove = (id) => {
+    pool.query(`
+        DELETE FROM todo
+        WHERE todo_id = $1
+        `, [id])
+
+}
+
+module.exports = {
+    create,
+    get,
+    remove
+}
