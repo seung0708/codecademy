1d173f1f3633e0ab5561234cd0109a4c1bb76c97
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-09-22 13:54:38 -0700

    completed added the functions in App.js

--- full-stack-path/off-platform-project/to-do-list/view/src/App.js
+++ full-stack-path/off-platform-project/to-do-list/view/src/App.js
@@ -9,11 +9,43 @@ const App = () => {
   const [todoList, setTodoList] = useState();
   const [error, setError] = useState();
 
-// Create a fetchTodos() function to update the View from Model using getTodos() function from Controller
+  // Create a fetchTodos() function to update the View from Model using getTodos() function from Controller
+  const fetchTodos = async () => {
+    const res = await getTodos(); 
+    if (res.error) {
+      setError(res.error.name)
+    }
+    setTodoList(res.data)
+  }
 
-// Create a handleDelete() function to remove to-do list with matching id
+  // Create a handleDelete() function to remove to-do list with matching id
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError();
+    const data = new FormData(e.currentTarget);
+    try {
+      data.set('description', todo.description);
+      data.set('created_at', `${new Date().toISOString()}`);
+      const newTodo = await createTodo(data);
+      if (newTodo.error) {
+        setError(newTodo.error);
+      }
+      setTodo({ description: '' });
+      fetchTodos();
+    } catch (err) {
+      setError(err);
+    }
+  };
+  // Create a handleSubmit() function to add new to-do list
+  const handleDelete = async (id) => {
+    try {
+      await removeTodo(id);
+      fetchTodos();
+    } catch (err) {
+      setError(err);
+    }
+  };
 
-// Create a handleSubmit() function to add new to-do list
 
   useEffect(() => {
     // Initialize todoList
