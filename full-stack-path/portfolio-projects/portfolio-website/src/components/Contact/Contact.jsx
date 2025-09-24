1a9958986524871bed43bc6202ea3ee460937a9f
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-09-24 15:51:12 -0700

    updated contact page

--- full-stack-path/portfolio-projects/portfolio-website/src/components/Contact/Contact.jsx
+++ full-stack-path/portfolio-projects/portfolio-website/src/components/Contact/Contact.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './Contact.css';
+import {Mail} from 'lucide-react'
 
 const Contact = () => {
-  return (
+    return (
     <section id="contact">
-            <div className="container">
-                <div className="contact_heading">
-                    <h2>Contact Me</h2>
+            <div className="contact-container">
+                <div className="form-container">
+                    <div className='left-container'>
+                        <h2>Let's Chat</h2>
+                        <p className="contact-text">Whether you have a question, want to start a project, or simply want to connect</p>
+                        <br/>
+                        <p>Feel free to send me a message</p>
+                    </div>
+                    <div className='right-container'>
+                       <form action="#">
+			                <h2 class="lg-view">Contact</h2>
+                			<input type="text" placeholder="Name *"  />
+                            <input type="email" placeholder="Email *" />
+			                <input type="text" placeholder="Company" />
+			                <input type="phone" placeholder="Phone" />
+                            <textarea rows="4" placeholder="Message"></textarea>
+			                <button>Submit</button>
+		                </form>
+                    </div>
+                </div>
+                
+                
+                {/* <div className="contact-cards">
+                    <div className='contact-card'>
+                        <Mail />
+                        <h3>Email: <span>seung.kim0708@gmail.com</span></h3>
+                    </div>
+
+                    <h2>Contact me!</h2>
                     <p>Say hello @ seung.kim0708@gmail.com</p>
                     <p>For my info, here's my <a href="./assets/General Resume (2).pdf">resume</a></p>
                     <div className="social_links">
@@ -17,28 +44,7 @@ const Contact = () => {
                             <img src="./assets/images/github.svg" />
                         </a>
                     </div>
-                </div>
-                <div className="contact_form">
-                    <form>
-                        <div className="contact_form_field">
-                            <label>Name:</label>
-                            <input type="text" id="name" name="name" required/>
-                        </div>
-                        <div className="contact_form_field">
-                            <label>Email:</label>
-                            <input type="email" id="email" name="email" required/>
-                        </div>
-                        <div className="contact_form_field">
-                            <label>Subject:</label>
-                            <input type="text" id="subject" name="subject" required/>
-                        </div>
-                        <div className="contact_form_field">
-                            <label>Message:</label>
-                            <textarea required></textarea>
-                        </div>
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
+                </div> */}
             </div>
         </section>
   )
