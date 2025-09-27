826899aa057146ad228ee0644908921fed3e570f
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-09-27 10:10:12 -0700

    updated contact component

--- full-stack-path/portfolio-projects/portfolio-website/src/components/Contact/Contact.jsx
+++ full-stack-path/portfolio-projects/portfolio-website/src/components/Contact/Contact.jsx
@@ -4,19 +4,29 @@ import {Mail} from 'lucide-react'
 
 const Contact = () => {
     return (
-    <section id="contact">
-            <div className="contact-container">
-                <div className="form-container">
-                    <div className='left-container'>
+        <div class="container">
+            <div class="form-container">
+                <div class="left-container">
+                    <div class="left-inner-container">
                         <h2>Let's Chat</h2>
-                        <p className="contact-text">Whether you have a question, want to start a project, or simply want to connect</p>
-                        <br/>
-                        <p>Feel free to send me a message</p>
+                        <p>Whether you have a question, want to start a project or simply want to connect.</p>
+                        <br />
+                        <p>Feel free to send me a message!</p>
                     </div>
-                    <div className='right-container'>
-                       <form action="#">
+                </div>
+                <div class="right-container">
+                    <div class="right-inner-container">
+                        <form action="#">
 			                <h2 class="lg-view">Contact</h2>
-                			<input type="text" placeholder="Name *"  />
+                            <h2 class="sm-view">Let's Chat</h2>
+                            
+			                <div class="social-container">
+				                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			                </div>
+                            
+                            <input type="text" placeholder="Name *"  />
                             <input type="email" placeholder="Email *" />
 			                <input type="text" placeholder="Company" />
 			                <input type="phone" placeholder="Phone" />
@@ -25,29 +35,10 @@ const Contact = () => {
 		                </form>
                     </div>
                 </div>
-                
-                
-                {/* <div className="contact-cards">
-                    <div className='contact-card'>
-                        <Mail />
-                        <h3>Email: <span>seung.kim0708@gmail.com</span></h3>
-                    </div>
-
-                    <h2>Contact me!</h2>
-                    <p>Say hello @ seung.kim0708@gmail.com</p>
-                    <p>For my info, here's my <a href="./assets/General Resume (2).pdf">resume</a></p>
-                    <div className="social_links">
-                        <a href="https://www.linkedin.com/in/seung-kim-8a3469133/">
-                            <img src="./assets/images/linkedin.png" />
-                        </a>
-                        <a href="https://github.com/seung0708">
-                            <img src="./assets/images/github.svg" />
-                        </a>
-                    </div>
-                </div> */}
             </div>
-        </section>
-  )
+        </div>
+    )
 }
 
 export default Contact
+
