b705190e54dd4b502a0829a3b4170b34b6534b42
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-10-02 17:54:28 -0700

    created database tables

---
+++ full-stack-path/portfolio-projects/e-commerce-app/models/order_items_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
