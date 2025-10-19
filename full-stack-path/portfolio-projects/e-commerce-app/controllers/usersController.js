fffac6f3918ec465de7cbd7b884477c248662948
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-10-19 13:28:10 -0700

    updated users controller

--- full-stack-path/portfolio-projects/e-commerce-app/controllers/usersController.js
+++ full-stack-path/portfolio-projects/e-commerce-app/controllers/usersController.js
@@ -19,38 +19,12 @@ export const getUserById = async (req, res) => {
 
 //get orders by user ID
 export const getOrdersByUserId = async (req, res) => {
-    const { userId } = req.session;
+    if (!req.isAuthenticated()) return res.status(401).json({ error: "Unauthorized" });
     try {
-        const orders = await pool.query('SELECT * FROM orders WHERE user_id = $1', [id]);
+        const orders = await pool.query('SELECT * FROM orders WHERE user_id = $1', [req.user.id]);
         res.status(200).json(orders.rows);
     } catch (error) {
         console.log('Error fetching orders by user ID:', error);
         res.status(500).json({ error: error.message });
     }
 }
-
-//create order for user
-export const createOrderForUser = async (req, res) => {
-    const { userId } = req.session;
-    const { quantity } = req.body;
-    try {
-        const newOrder = await pool.query(
-            'INSERT INTO orders (user_id, total, status) VALUES ($1, $2, $3) RETURNING *', [id, total, status]
-        );
-        
-        const product = await getProductById({ params: { id: productId } });
-        if (product.status !== 200) {
-            return res.status(400).json({ error: 'Invalid product ID' });
-        }   
-
-        const orderItem = await pool.query(
-            'INSERT INTO order_items (order_id, product_id, quantity, price) VALUES ($1, $2) RETURNING *', 
-            [newOrder.rows[0].id, product.rows[0].id, 2, product.rows[0].price]
-        );
-        res.status(201).json({ order: newOrder.rows[0], orderItem: orderItem.rows[0] });
-    } catch (error) {
-        console.log('Error creating order for user:', error);
-        res.status(500).json({ error: error.message });
-    }
-}
-
