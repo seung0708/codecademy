ed22ee72b97db4e7525c13c5eabdf4d73b8e5b36
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-10-19 13:26:26 -0700

    updated users route

--- full-stack-path/portfolio-projects/e-commerce-app/routes/users.js
+++ full-stack-path/portfolio-projects/e-commerce-app/routes/users.js
@@ -1,18 +1,13 @@
 import express from "express";
 const userRouter = express.Router();
 
-import { getUserById, getOrdersByUserId, createOrderForUser } from "../controllers/usersController.js";
+import { authorizedUser } from "../middleware/auth.js";
 
-userRouter.get("/me", getUserById);
+import { getUserById, getOrdersByUserId } from "../controllers/usersController.js";
 
-// userRouter.get("/users/:id/orders", (req, res) => {
-//     const userId = req.params.id;
-//     res.status(200).send("User orders found");
-// });
+userRouter.get("/me", authorizedUser, getUserById);
+userRouter.get("/me/orders", authorizedUser, getOrdersByUserId);
 
-// userRouter.post("/users/:id/orders", (req, res) => {
-//     const userId = req.params.id;
-//     res.status(201).send("Order created");
-// });
+
 
 export default userRouter;
