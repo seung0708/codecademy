b821cb8d980a865f15ef8329cd392179325d1f55
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-10-22 15:30:29 -0700

    finished header component for now

--- full-stack-path/portfolio-projects/e-commerce-app/view/src/components/Header.jsx
+++ full-stack-path/portfolio-projects/e-commerce-app/view/src/components/Header.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 
 import Container from "./Container";
 import { Link } from "react-router-dom";
-import { ShoppingBagIcon, Bars3Icon, UserIcon, ArrowRightIcon  } from "@heroicons/react/24/outline";
+import { ShoppingBagIcon, Bars3Icon } from "@heroicons/react/24/outline";
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+    
     return (
         <Container className="header">
             <div className="header__left">
                 <Bars3Icon className="header__hamburger__icon" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} />
-                <Link to="/">Fashion Store</Link>
+                <Link to="/" className="header__logo">Fashion Store</Link>
                 <nav className="header__nav__desktop">
                     <ul>
                         <li>
@@ -27,15 +29,20 @@ const Header = () => {
             </div>
             <div className="header__right">
                 <div className="header__actions">
+                {isUserLoggedIn ? (
+                    <Link className="header__actions__link__logout" to="/logout">Logout</Link>
+                ) : (
+                  <>
+                    <Link className="header__actions__link" to="/login">Login</Link> /
+                    <Link className="header__actions__link" to="/register">Register</Link>
+                  </>
+                )}
                     <div className="header__actions__cart">
                         <Link className="header__actions__link" to="/cart">
                             <ShoppingBagIcon className="header__actions__link__icon"  />
                         </Link>
                         <span className="header__actions__cart__count">0</span>
                     </div>
-                    
-                    <Link className="header__actions__link" to="/login">Login</Link>/
-                    <Link className="header__actions__link" to="/register">Register</Link>
                 </div>
             </div>
            
