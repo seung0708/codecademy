27b9141c0fceaf64c8fa882209ad196d3678ed8c
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-11-05 16:12:30 -0800

    decided not to do the animation for now and focus on completing the portfolio project by making it responsive

--- full-stack-path/portfolio-projects/portfolio-website/src/components/Hero/Hero.jsx
+++ full-stack-path/portfolio-projects/portfolio-website/src/components/Hero/Hero.jsx
@@ -16,24 +16,6 @@ const icons = [
 ];
 
 const Hero = () => {
-  const containerRef = useRef(null)
-  const [scrollTop, setScrollTop] = useState(0)
-
-  const handleScroll = () => {
-    if (containerRef.current) {
-      setScrollTop(containerRef.current.scrollTop)
-    }
-  };
-
-  const fadeStart = 50; // start fading after 50px scrolled
-  const fadeEnd = 200;  // fully faded at 200px
-
-  let fadeOutOpacity = 1 - (scrollTop - fadeStart) / (fadeEnd - fadeStart);
-  let fadeInOpacity = (scrollTop - fadeStart) / (fadeEnd - fadeStart);
-
-  fadeOutOpacity = Math.min(Math.max(fadeOutOpacity, 0), 1);
-  fadeInOpacity = Math.min(Math.max(fadeInOpacity, 0), 1);
-
   return (
     <section className="hero">
       <div className="icons-overlay">
@@ -47,44 +29,8 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div 
-        className="hero-content"
-        ref={containerRef}
-        onScroll={handleScroll}
-        style={{
-          height: "300px"
-          
-        }}
-      >
-        <div 
-          className="hero-image"
-          style={{
-            position: "sticky",
-            top: "25px",
-            transition: "opacity 0.3s ease",
-            opacity: fadeOutOpacity
-          }}
-        >
-          <img src="/assets/images/IMG_20190312_191356_018.jpg" alt="Hero Image" />
-        </div>
-        <div 
-          className="hero-text"
-          style={{
-            position: "sticky",
-            transition: "opacity 0.3s ease",
-            opacity: fadeInOpacity
-          }}
-        >
-          <h3 className="text-line">
-            Hello, my name is Seung Kim
-          </h3>
-          <h3
-            className="text-line"
-          >
-            I am a Full-Stack developer
-          </h3>
-        </div>
-        <div style={{ height: "500px" }}></div>
+      <div className="hero-image">
+        <img src="/assets/images/IMG_20190312_191356_018.jpg" alt="Hero Image" />
       </div>
     </section>
   );
